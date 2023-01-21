cb3931357a82c95de65fef0d0e449bf31b94277e
owolabi-develop <owolabidevelop84@gmail.com>
2023-01-21 13:08:19 -0800

    mg to digitalOcean

--- music/views.py
+++ music/views.py
@@ -1,13 +1,13 @@
 
 from django.shortcuts import render
-import json
+
 import spotipy
 from spotipy.oauth2 import SpotifyClientCredentials
 # Create your views here.
 from musiclover.settings import CLIENT_ID,CLIENT_SECRET
 sp = spotipy.Spotify(auth_manager=SpotifyClientCredentials(client_id=CLIENT_ID,client_secret=CLIENT_SECRET))
-
-
+from pprint import pprint
+#main page........
 def baseMusic(request):
    
     playlists = sp.user_playlists('spotify')
@@ -17,7 +17,7 @@ def baseMusic(request):
    
     return render(request,'music/base.html',{'article':playlists['items']})
 
-
+#music search features....................
 def music_search(request):
     artists_query = request.GET.get('musicQuery',None)
     results = sp.search(q=artists_query,type='artist')
@@ -27,7 +27,7 @@ def music_search(request):
         artists_tracks = sp.artist_top_tracks(artist_id=uri)
         artists_album = sp.artist_albums(artist_id=uri,album_type='single',limit=5)
         artists_data = sp.artist(artist_id=uri)
-        print(artists_album['items'])
+        
     else:
         return render(request,'music/search.html',{'track':'','artists_data':''})
       
